0950de3c0b3149f1efa484b90cc9d9875ee53a39
Alex Caulfield <alex.caulfield28@gmail.com>
2020-10-17 17:03:31 -0400

    cleaning up footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import Footer from './components/footer';
 import { BrowserRouter as Router} from 'react-router-dom';
 import AppRouting from "./components/app_routing";
 
